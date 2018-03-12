9ef8d08603f5269e39fff3779ac1913ea057fb4a
Salim Fgaier <fgaier.salim@gmail.com>
2018-03-12 12:00:11 -0700

    first commit with app ready for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'mysite-backup/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
